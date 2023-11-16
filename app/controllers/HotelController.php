e56a791788127ef29fd5a8884122fd409a97f21a
Janaalkaseer <janaalkasier@gmail.com>
2023-11-16 20:05:17 +0300

    jana airlines

---
+++ app/controllers/HotelController.php
@@ -0,0 +1,67 @@
+<?php
+// namespace control_hotel;
+// use model_hotel;
+
+class HotelController {
+    private $model;
+
+    public function __construct($model) {
+        $this->model = $model;
+    }
+    public function addHotel() {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $city_id=$_POST['city_id'];
+        $name=$_POST['name'];
+        $phone=$_POST['phone'];
+        $data = [
+            'city_id' =>$city_id,
+            'name' =>$name ,
+            'phone' => $phone ,
+            
+        ];
+
+        if ($this->model->addHotel($data)) {
+           echo "Done .";
+         }
+    }
+}
+
+
+public function updateHotel($id){
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $id=$_POST['id'];
+        $city_id=$_POST['city_id'];
+        $name=$_POST['name'];
+        $phone=$_POST['phone'];
+        $data = [
+            'city_id' =>$city_id,
+            'name' =>$name ,
+            'phone' => $phone ,
+          
+        ];
+
+        if ($this->model->updateHotel($id, $data)) {
+            echo "Hotel updated successfully!";}
+           else {
+             echo "Failed to update user.";
+         }
+
+    }}
+
+public function deleteHotel($id){
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $id=$_POST['id'];}
+        if ($this->model->deleteHotel($id)) {
+            echo "Delete done";
+    }
+
+}
+
+}
+?>
+
+
+    
+    
+    
+    
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+// require_once __DIR__ . '/app/controllers/CityController.php';
+// require_once __DIR__ .'/app/models/CityModel.php';
+require_once __DIR__ . '/app/controllers/HotelController.php';
+require_once __DIR__ .'/app/models/HotelModel.php';
+require_once __DIR__ .'/config/config.php';
+require_once __DIR__ . '/lib/DB/MysqliDb.php';
+// use model_city;
+// use control_city;
+
+$config = require 'config/config.php';
+$db = new MysqliDb(
+    $config['db_host'],
+    $config['db_user'],
+    $config['db_pass'],
+    $config['db_name']
+);
+
+
+// $request=$_SERVER['REQUEST_URI'];
+// define('BASE_PATH','/air-lines/');
+//var_dump($request);
+
+//$model = new CityModel($db);
+//$controller = new CityController($model);
+
+//$controller->index();
+//$controller->addCity();
+//$controller->updateCity($_POST['id']);
+//$controller->deleteCity($_POST['id']); 
+                 
+////////////Hotels
+$model = new HotelModel($db);
+$controller = new HotelController($model);
+// $controller->index();
+//$controller->addHotel();
+ //$controller->updateHotel($_POST['id']);
+//$controller->deleteHotel($_POST['id']); 
+
+?>
