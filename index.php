afda94a2e100146c2e0ba081ba91a2b544c5c648
Mo-fawaz <149706222+Mo-fawaz@users.noreply.github.com>
2023-11-18 07:19:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,1 @@
-<?php
-require_once __DIR__ . '/app/controllers/TicketController.php';
-require_once __DIR__ .'/app/models/TicketModel.php';
-require_once __DIR__ .'/config/config.php';
-require_once __DIR__ . '/lib/DB/MysqliDb.php';
-$config = require 'config/config.php';
-$db = new MysqliDb(
-    $config['db_host'],
-    $config['db_user'],
-    $config['db_pass'],
-    $config['db_name']
-);
-use TicketController\ticketcontroller;
-use TicketModel\ticketmodel ;
-$model = new TicketModel($db);
-$controller = new TicketController($model);
-$controller->updatTicket();
+
